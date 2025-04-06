99249f85e95cf7f6f029b79144b1ff9cd344ce0c
Daniil Lyasman <danlaysma45.work@gmail.com>
2025-04-06 13:37:59 +0600

    edit oxygen sys

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using System;
 
 public static class PlayerStats
 {
   public static float speedY = 2f;
   public  static float  speedX = 0.15f;
+  public static Int16 oxygen = 1000;
 }
