910bbb2a88748ffb99cc6649c51ab652600d3e47
Egor Oshchepkov <oschepkov.egor55@gmail.com>
2025-04-06 11:39:54 +0600

    feat: add death

--- Assets/Scripts/MovingPlayer.cs
+++ Assets/Scripts/MovingPlayer.cs
@@ -3,25 +3,30 @@ using UnityEngine;
 
 public class Moving : MonoBehaviour
 {
+    private Boolean isDeath = false;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Shark"))
         {
-            gameObject.SetActive(false);
+            isDeath = true;
+            gameObject.GetComponent<SpriteRenderer>().color = Color.red;
         }
     }
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0, -0.05f, 0);
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        if (!isDeath)
         {
-            transform.position += new Vector3(0.15f, 0, 0);
-        }
+            transform.position += new Vector3(0, -0.05f, 0);
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += new Vector3(0.15f, 0, 0);
+            }
 
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.position += new Vector3(-0.15f, 0, 0);
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position += new Vector3(-0.15f, 0, 0);
+            }
         }
     }
 }
--- Assets/Scripts/MovingShark.cs
+++ Assets/Scripts/MovingShark.cs
@@ -1,13 +1,23 @@
 using System;
 using UnityEngine;
+using System.Collections;
 
 public class MovingShark : MonoBehaviour
 {
     GameObject player; 
     Int16 direction;
+    private Boolean isEating = false;
     void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isEating = true;
+            StartCoroutine(TimeoutCoroutine(1.5f)); // Ждём 3 секунды
+        }
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,19 +25,33 @@ public class MovingShark : MonoBehaviour
         if (player.transform.position.x > transform.position.x)
         {
             direction = 1;
+            transform.Rotate(0,0,0);
         }
         else
         {
             direction = -1;
+            transform.Rotate(0,200,0);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Math.Abs(transform.position.y - player.transform.position.y) < 6)
+        if (!isEating)
         {
-            transform.position += new Vector3(direction * 0.07f, -0.02f, 0);
+            if (Math.Abs(transform.position.y - player.transform.position.y) < 6)
+            {
+                transform.position += new Vector3(direction * 0.07f, -0.02f, 0);
+            }
         }
+    }
+    
+    IEnumerator TimeoutCoroutine(float delay)
+    {
+        Debug.Log("Начало таймаута");
+        yield return new WaitForSeconds(delay);
+        Debug.Log("Таймаут завершён");
+        isEating = false;
+        // Здесь код, который нужно выполнить после таймаута
     }
 }
